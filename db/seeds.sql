d72a9450c262ccde70b341a988b4c2dec37f2cd4
Tony Rivas <tonyjrivas@gmail.com>
2020-10-30 13:28:40 -0600

    added initial files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
